26d3ff518c3a34025603e9a7378761da38b0a8a8
aep <aepsaprul18@gmail.com>
2019-09-20 11:02:49 +0700

    instal laravel

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,167 @@
+<link href="{{ asset('client/css/bootstrap.css') }}" rel="stylesheet" id="bootstrap-css">
+<script src="{{ asset('client/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('client/js/jquery-2.0.0.min.js') }}"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Warung Mitra | Register</title>
+        <!-- Font awesome 5 -->
+        <link href="{{ asset('client/fonts/fontawesome/css/fontawesome-all.min.css') }}" type="text/css" rel="stylesheet">
+        <style>
+                /* Coded with love by Mutiullah Samim */
+            body,
+            html {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                background: #60a3bc !important;
+            }
+            .user_card {
+                height: 700px;
+                width: 350px;
+                margin-top: 10%;
+                margin-bottom: 20px;
+                background: #fff;
+                position: relative;
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                padding: 20px;
+                box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                -webkit-box-shadow: 0 4px 28px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                -moz-box-shadow: 0 4px 28px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                border-radius: 5px;
+
+            }
+            .brand_logo_container {
+                position: absolute;
+                height: 170px;
+                width: 170px;
+                top: -75px;
+                border-radius: 50%;
+                background: #fff;
+                padding: 10px;
+                text-align: center;
+            }
+            .brand_logo {
+                height: 150px;
+                width: 150px;
+                border-radius: 50%;
+                border: 2px solid white;
+            }
+            .form_container {
+                margin-top: 100px;
+            }
+            .login_btn {
+                width: 100%;
+                background: #228B22 !important;
+                color: white !important;
+            }
+            .login_btn:focus {
+                box-shadow: none !important;
+                outline: 0px !important;
+            }
+            .login_container {
+                padding: 0 2rem;
+            }
+            .input-group-text {
+                background: #228B22 !important;
+                color: white !important;
+                border: 0 !important;
+                border-radius: 0.25rem 0 0 0.25rem !important;
+            }
+            .input_user,
+            .input_pass:focus {
+                box-shadow: none !important;
+                outline: 0px !important;
+            }
+            .custom-checkbox .custom-control-input:checked~.custom-control-label::before {
+                background-color: #c0392b !important;
+            }
+            .label-persetujuan {
+                font-size: 14px;
+            }
+        </style>
+    </head>
+    <!--Coded with love by Mutiullah Samim-->
+    <body>
+        <div class="container h-100">
+            <div class="d-flex justify-content-center h-100">
+                <div class="user_card">
+                    <div class="d-flex justify-content-center">
+                        <div class="brand_logo_container">
+                            <img src="{{ asset('client/images/logos/logo-wm.png') }}" class="brand_logo" alt="Logo">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center form_container">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="nama" class="form-control input_user" value="" placeholder="nama lengkap">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                </div>
+                                <input type="text" name="" class="form-control input_user" value="" placeholder="email">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                                </div>
+                                <input type="text" name="" class="form-control input_user" value="" placeholder="nomor hp">
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                <label class="form-check-label" for="inlineRadio1">Laki-Laki</label>
+                            </div>
+                            <div class="form-check form-check-inline mb-3">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                <label class="form-check-label" for="inlineRadio2">Perempuan</label>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-address-card"></i></span>
+                                </div>
+                                <input type="text" name="" class="form-control input_user" value="" placeholder="alamat">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="username" class="form-control input_user" value="" placeholder="username">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input type="password" name="" class="form-control input_pass" value="" placeholder="password">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="konfirmasi password">
+                            </div>
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label label-persetujuan" for="customControlInline">Saya telah membaca dan menyetujui <a href="#">Aturan Penggunaan</a> dan <a href="#">Kebijakan Privasi</a> Warung Mitra</label>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-center mt-3">
+                                <button type="submit" name="button" class="btn login_btn btn-block">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,277 @@
+<!-- Copyright &copy 2018 http://bootstrap-ecommerce.com -->
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Warung Mitra') }}</title>
+
+    <link href="{{ asset('client/css/bootstrap.css') }}" rel="stylesheet" type="text/css"/>
+
+    <!-- Font awesome 5 -->
+    <link href="{{ asset('client/fonts/fontawesome/css/fontawesome-all.min.css') }}" type="text/css" rel="stylesheet">
+
+    <!-- plugin: owl carousel  -->
+    <link href="{{ asset('client/plugins/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('client/plugins/owlcarousel/assets/owl.theme.default.css') }}" rel="stylesheet">
+
+    <!-- plugin: slickslider -->
+    <link href="{{ asset('client/plugins/slickslider/slick.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('client/plugins/slickslider/slick-theme.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- custom style -->
+    <link href="{{ asset('client/css/ui.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('client/css/responsive.css') }}" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+
+    @yield('style')
+
+    <style>
+        .logo-text {
+            font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        }
+    </style>
+</head>
+<body>
+    {{-- <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        
+    </div> --}}
+
+    <header class="section-header">
+        <section class="header-main">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5-24 col-sm-5 col-4">
+                        <div class="brand-wrap">
+                            <img class="logo" src="{{ asset('client/images/logos/logo-wm.png') }}">
+                            <h2 class="logo-text">Warung Mitra</h2>
+                        </div> <!-- brand-wrap.// -->
+                    </div>
+                    <div class="col-lg-13-24 col-sm-12 order-3 order-lg-2">
+                        <form action="#">
+                            <div class="input-group w-100">
+                                <input type="text" class="form-control" style="width:60%;" placeholder="Search">                        
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form> <!-- search-wrap .end// -->
+                    </div> <!-- col.// -->
+                    <div class="col-lg-6-24 col-sm-7 col-8  order-2  order-lg-3">
+                        <div class="d-flex justify-content-end">
+                            <div class="widget-header">
+                                @guest
+                                    <small class="title text-muted">Selamat Datang</small>
+                                    <div>
+                                        <a href="{{ route('login') }}">Login</a> <span class="dark-transp"> | </span>
+                                        <a href="{{ route('register') }}"> Register</a>
+                                    </div>
+                                @else
+                                    <small class="title text-muted">aa</small>
+                                    <div>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                                Logout
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                @endauth
+                                
+                            </div>
+                            <a href="#" class="widget-header border-left pl-3 ml-3 shopping-cart">
+                                <div class="icontext">
+                                    <div class="icon-wrap icon-sm round border"><i class="fa fa-shopping-cart"></i></div>
+                                </div>
+                                <span class="badge badge-pill badge-danger notify notif-cart" id="notifcart"></span>
+                            </a>
+                        </div> <!-- widgets-wrap.// -->
+                    </div> <!-- col.// -->
+                </div> <!-- row.// -->
+            </div> <!-- container.// -->
+        </section> <!-- header-main .// -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <div class="container"> 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="/">Beranda</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Kontak</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><strong>All category</strong></a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown07">
+                                <a class="dropdown-item" href="#">Bahan Masak</a>
+                                <a class="dropdown-item" href="#">Barang Paketan</a>
+                                <a class="dropdown-item" href="#">Kebutuhan Bayi</a>
+                                <a class="dropdown-item" href="#">Kesehatan & Kecantikan</a>
+                                <a class="dropdown-item" href="#">Kopi</a>
+                                <a class="dropdown-item" href="#">Makanan</a>
+                                <a class="dropdown-item" href="#">Mie</a>
+                                <a class="dropdown-item" href="#">Minuman</a>
+                                <a class="dropdown-item" href="#">Pakaian</a>
+                                <a class="dropdown-item" href="#">Perlengkapan Mandi</a>
+                                <a class="dropdown-item" href="#">Perlengkapan Rumah Tangga</a>
+                                <a class="dropdown-item" href="#">Renceng</a>
+                                <a class="dropdown-item" href="#">Sembako</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div> <!-- collapse .// -->
+            </div> <!-- container .// -->
+        </nav>
+    </header> <!-- section-header.// -->
+
+    <main class="py-4">
+        @yield('content')
+    </main>
+
+    <!-- ========================= FOOTER ========================= -->
+    <footer class="section-footer bg-secondary">
+        <div class="container">
+            <section class="footer-top padding-top">
+                <div class="row">
+                    <aside class="col-sm-3 col-md-3 white">
+                        <h5 class="title">Customer Services</h5>
+                        <ul class="list-unstyled">
+                            <li> <a href="#">Help center</a></li>
+                            <li> <a href="#">Money refund</a></li>
+                            <li> <a href="#">Terms and Policy</a></li>
+                        </ul>
+                    </aside>
+                    <aside class="col-sm-3  col-md-3 white">
+                        <h5 class="title">My Account</h5>
+                        <ul class="list-unstyled">
+                            <li> <a href="#"> User Login </a></li>
+                            <li> <a href="#"> User register </a></li>
+                            <li> <a href="#"> Account Setting </a></li>
+                            <li> <a href="#"> My Orders </a></li>
+                            <li> <a href="#"> My Wishlist </a></li>
+                        </ul>
+                    </aside>
+                    <aside class="col-sm-3  col-md-3 white">
+                        <h5 class="title">About</h5>
+                        <ul class="list-unstyled">
+                            <li> <a href="#"> Our history </a></li>
+                            <li> <a href="#"> How to buy </a></li>
+                            <li> <a href="#"> Delivery and payment </a></li>
+                            <li> <a href="#"> Advertice </a></li>
+                            <li> <a href="#"> Partnership </a></li>
+                        </ul>
+                    </aside>
+                    <aside class="col-sm-3">
+                        <article class="white">
+                            <h5 class="title">Kontak</h5>
+                            <p>
+                                <strong>Phone: </strong> +123456789 <br> 
+                                <strong>Phone: </strong> +123456789 <br> 
+                                <strong>WhatsApp: </strong> +123456789 
+                            </p>
+
+                            <div class="btn-group white">
+                                <a class="btn btn-facebook" title="Facebook" target="_blank" href="#"><i class="fab fa-facebook-f  fa-fw"></i></a>
+                                <a class="btn btn-instagram" title="Instagram" target="_blank" href="#"><i class="fab fa-instagram  fa-fw"></i></a>
+                                <a class="btn btn-youtube" title="Youtube" target="_blank" href="#"><i class="fab fa-youtube  fa-fw"></i></a>
+                            </div>
+                        </article>
+                    </aside>
+                </div> <!-- row.// -->
+                <br> 
+            </section>
+            <section class="footer-bottom row border-top-white">
+                <div class="col-sm-6"> 
+                    <p class="text-white-50">
+                        Copyright &copy 2019 <a href="#" class="text-white-50">warungmitra.com</a> <br>  
+                    </p>
+                </div>
+            </section> <!-- //footer-top -->
+        </div><!-- //container -->
+    </footer>
+    <!-- ========================= FOOTER END // ========================= -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('client/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
+
+    <!-- Bootstrap4 files-->
+    <script src="{{ asset('client/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('client/plugins/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('client/plugins/slickslider/slick.min.js') }}"></script>
+    <!-- custom javascript -->
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5d646faa77aa790be330de31/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
+    @yield('script')
+</body>
+</html>
